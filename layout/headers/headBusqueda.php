50eb24147258c17734f4d770db15658677b41ff6
Ernesto Rubio F <neto.r27@gmail.com>
2012-09-17 22:45:37 -0500

    Remove Buscador

--- layout/headers/headBusqueda.php
+++
@@ -1,2 +0,0 @@
-<link rel="stylesheet" href="/layout/css/listaCursos.css" />
-<link rel="stylesheet" href="/layout/css/buscador.css"/>
--- layout/headers/headIndex.php
+++ layout/headers/headIndex.php
@@ -1,6 +1,5 @@
 <link rel="stylesheet" href="/layout/css/index.css" />
 <link rel="stylesheet" href="/layout/css/listaCursos.css" />
-<link rel="stylesheet" href="/layout/css/buscador.css"/>
 <link rel="stylesheet" href="/layout/css/comentarios.css"/>
 <script src="/js/jquery.pagination.js" type="text/javascript"></script>
 <script src="/js/indexDocumentReady.js" type="text/javascript"></script>
