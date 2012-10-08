9c4cee8904162a12d8487af1bc28dcf8697154fd
Ernesto Rubio F <neto.r27@gmail.com>
2012-10-08 17:32:09 -0500

    Validar IE

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+function using_ie() {
+    $u_agent = $_SERVER['HTTP_USER_AGENT'];
+    $ub = False;
+    if (preg_match('/MSIE/i', $u_agent)) {
+        $ub = True;
+    }
+
+    return $ub;
+}
+
+if (using_ie()) {
+    echo 'ES INTERNET EXPLORER';    
+}else{
+    echo 'NO es internet explorer';
+}
+?>
