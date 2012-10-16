1124b77aa516119330ba100dd717efb9e6e9d28a
Ernesto Rubio F <neto.r27@gmail.com>
2012-10-16 18:17:34 -0500

    Remove footer links

--- layout/foot.php
+++ layout/foot.php
@@ -3,9 +3,9 @@
     <div class="centerer">
         <div>
             <ul>
-                <li><a href="mailto:contacto@unova.mx">Contacto</a></li>
+<!--                <li><a href="mailto:contacto@unova.mx">Contacto</a></li>
                 <li><a href="/politicaDePrivacidad">Politica de Privacidad</a></li>
-                <li><a href="/nosotros">Nosotros</a></li>
+                <li><a href="/nosotros">Nosotros</a></li>-->
             </ul>
         </div>
     </div>
