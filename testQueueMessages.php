8a95d61bf1e84b7b2804c47b640b3297e6d583ba
Ernesto Rubio F <neto.r27@gmail.com>
2012-10-24 14:02:26 -0500

    Publicar directamente a la cola de mensajes

--- testQueueMessages.php
+++ testQueueMessages.php
@@ -12,16 +12,21 @@ if (!isset($_GET['a'])) {
     $accion = $_GET['a'];
 }
 
-if ($accion == "publicar") {    
-    $colaMensajes->push("".rand(0, 200));
+if ($accion == "publicar") {
+    if (isset($_GET['i'])) {
+        $id = $_GET['i'];
+    } else {
+        $id = rand(0, 200);
+    }
+    $colaMensajes->push("" . $id);
     echo 'se publico un job<br>';
 }
 
 if ($accion == "leer") {
     $job = $colaMensajes->pop();
     if ($job == "") {
-        echo 'no hay datos en job, suponemos time_out<br>';        
-    }else{
+        echo 'no hay datos en job, suponemos time_out<br>';
+    } else {
         echo $job->getData() . '<br><br>';
         $colaMensajes->deleteJob($job);
     }
