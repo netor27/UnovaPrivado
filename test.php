a18866ad82ddfd8c40fc0bff7cd8a2949e22de60
Ernesto Rubio F <neto.r27@gmail.com>
2012-11-07 19:07:58 -0600

    add test.php

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+$path = "c:/wamp";
+if(isset($_POST['path'])){
+    $path = $_POST['path'];
+}
+
+require_once 'funcionesPHP/funcionesGenerales.php';
+echo 'Path ' . $path . '<br><br>';
+echo 'Espacio total en disco = ' . bytesToString(disk_total_space($path),4). '<br>';
+echo 'Espacio disponible     = ' . bytesToString(disk_free_space($path),4).'<br>';
+
+?>
+
+<br><br><br>
+<form action="test.php" method="post">
+    <input type="text" name="path"/>
+</form>
+?>
