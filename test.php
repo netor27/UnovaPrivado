0d39b0fca128a79c662f5b22b3f3558927155a34
Ernesto Rubio F <neto.r27@gmail.com>
2013-02-27 22:49:19 -0600

    Remove modulo transformación de archivos

--- test.php
+++ test.php
@@ -1,1 +1,17 @@
 <?php
+require_once 'funcionesPHP/funcionesGenerales.php';
+require_once 'modulos/aws/modelos/sqsModelo.php';
+
+//$message = '{"bucket":"unovaTesting","key":"porTransformar\/380269367483a031f1557667400c5af70fb53469cd721188ee3593123a2678f74c3d323d62720ac95a490deba8e6b438b6dc66dda161cb312c3e2d5fdab92860a83eee29176811474254c4.mp3","tipo":4,"host":"privado.localhost"}';
+//AddMessageToQueue($message);
+
+$mensaje = readMessageFromQueue();
+
+print_r($mensaje);
+echo '<br>';
+$body = json_decode($mensaje['Body']);
+echo 'bucket => ' . $body->bucket;
+echo '<br>';
+print_r($body);
+echo '<br>';    
+deleteMessageFromQueue($mensaje['ReceiptHandle']);
