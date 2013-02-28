b922e735d5730c1bebdba9cf53cae5c97db095c4
Ernesto Rubio F <neto.r27@gmail.com>
2013-02-28 15:48:37 -0600

    Configuración de conexión de base de datos por medio de variables de servidor

--- bd/conex.php
+++ bd/conex.php
@@ -1,30 +1,46 @@
 <?php
+
 require_once('bd/bd.php');
-try{	
-	$BDhost = 'localhost';
-	$BDbase = 'dbunovaprivado';
-	//$BDusuario = 'root';
-	//$BDpswd = 'root';
-	
-	$BDusuario = 'UnoVaUser';
-	$BDpswd = 'dbUnovaPass2012';
-        global $conex;
-	$conex = new Configpdo('mysql',$BDhost,$BDbase,$BDusuario,$BDpswd);
-}catch(PDOException $e){
+try {
+    if (isset($_SERVER['PARAM1']))
+        $BDhost = $_SERVER['PARAM1'];
+    else
+        $BDhost = 'localhost';
+    
+    if (isset($_SERVER['PARAM2']))
+        $BDbase = $_SERVER['PARAM2'];
+    else
+        $BDbase = 'dbunovaprivado';
+    
+    if (isset($_SERVER['PARAM3']))
+        $BDusuario = $_SERVER['PARAM3'];
+    else
+        $BDusuario = 'UnoVaUser';
+    
+    if (isset($_SERVER['PARAM4']))
+        $BDpswd = $_SERVER['PARAM4'];
+    else
+        $BDpswd = 'dbUnovaPass2012';
+    
+    global $conex;
+    $conex = new Configpdo('mysql', $BDhost, $BDbase, $BDusuario, $BDpswd);
+} catch (PDOException $e) {
     echo "ocurrió un error con la base de datos";
 }
-function beginTransaction(){
+
+function beginTransaction() {
     global $conex;
     $conex->beginTransaction();
 }
 
-function commitTransaction(){
+function commitTransaction() {
     global $conex;
     $conex->commit();
 }
 
-function rollBackTransaction(){
+function rollBackTransaction() {
     global $conex;
     $conex->rollBack();
 }
+
 ?>
