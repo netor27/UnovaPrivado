90c315e3dcbd4f9581e6c89ab7bcb672191a5372
Ernesto Rubio F <neto.r27@gmail.com>
2013-03-02 18:35:36 -0600

    Ending tests

--- test.php
+++ test.php
@@ -1,8 +1,11 @@
 <?php
 //require_once 'funcionesPHP/funcionesGenerales.php';
 //require_once 'modulos/aws/modelos/sqsModelo.php';
-
-//$message = '{"bucket":"unovaTesting","key":"porTransformar\/380269367483a031f1557667400c5af70fb53469cd721188ee3593123a2678f74c3d323d62720ac95a490deba8e6b438b6dc66dda161cb312c3e2d5fdab92860a83eee29176811474254c4.mp3","tipo":4,"host":"privado.localhost","idClase":115}';
+//
+//$message = '{"bucket":"unovaTesting","key":"porTransformar\/audio.mp3","tipo":4,"host":"privado.localhost","idClase":115}';
+//AddMessageToQueue($message);
+//
+//$message = '{"bucket":"unovaTesting","key":"porTransformar\/video.mov","tipo":0,"host":"privado.localhost","idClase":115}';
 //AddMessageToQueue($message);
 
 //$mensaje = readMessageFromQueue();
@@ -15,11 +18,11 @@
 //print_r($body);
 //echo '<br>';    
 //deleteMessageFromQueue($mensaje['ReceiptHandle']);
-
-echo $_GET['msg'];
-echo '<br>';
-echo '<br>';
-echo '<br>';
-$msg = $_GET['msg'];
-$msg = json_decode($msg);
-print_r($msg);
+//
+//echo $_GET['msg'];
+//echo '<br>';
+//echo '<br>';
+//echo '<br>';
+//$msg = $_GET['msg'];
+//$msg = json_decode($msg);
+//print_r($msg);
