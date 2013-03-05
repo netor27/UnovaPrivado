3b84bd627d408fb3cea2f1389f1c75e810d80f49
Ernesto Rubio F <neto.r27@gmail.com>
2013-03-05 00:22:30 -0600

    Test domain names

--- test.php
+++ test.php
@@ -1,20 +1,12 @@
 <?php
 
 require_once 'funcionesPHP/funcionesGenerales.php';
-require_once 'modulos/aws/modelos/sesModelo.php';
 
-$text = "Este es un texto de prueba";
-$html = "Este es el html Los mails con html funcionan";
-$subject = "Mail de prueba";
-$from = "Unova-noreply@unova.mx";
-
-$to[] = "neto.r27@gmail.com";
-$to[] = "neto_r27@hotmail.com";
-$to[] = "calvises@yahoo.com";
-
-if (sendMail($text, $html, $subject, $from, $to)) {
-    echo 'Se envio el correo';
-} else {
-    echo '<br>';
-    echo 'Error al enviar';
-}
+echo 'getUrl() => ' . getUrl();
+echo '<br>';
+echo 'getDomainName() => ' . getDomainName();
+echo '<br>';
+echo 'getRequestUri() => ' . getRequestUri();
+echo '<br>';
+echo 'getServerRoot() => ' . getServerRoot();
+echo '<br>';
