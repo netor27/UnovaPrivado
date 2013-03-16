5606a08e7bebf69af1b7d26e0d388c4407a71c0f
Ernesto Rubio F <neto.r27@gmail.com>
2013-03-16 13:18:25 -0600

    footer background

--- layout/foot.php
+++ layout/foot.php
@@ -1,15 +1,19 @@
 </div>
 </div>
-<footer id="site-footer" >
-    <div class="centerer">
-        <div>
-            <ul>
-<!--                <li><a href="mailto:contacto@unova.mx">Contacto</a></li>
-                <li><a href="/politicaDePrivacidad">Politica de Privacidad</a></li>
-                <li><a href="/nosotros">Nosotros</a></li>-->
-            </ul>
+<div id="e_footer">    
+<!--    <strong>
+        <div id="pushfooter">
+            <footer id="footer">
+                <div class="container">
+                    <div class="row-fluid copyright">
+                        <div class="span6 offset3 copy">
+                            <p>© 2013 Unova · <a href="mailto:contacto@unova.mx">Contacto</a> · <a href="#" data-toggle="modal">Privacidad</a> · <a href="#">Terminos y condiciones</a></p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
-    </div>
-</footer>
+    </strong>-->
+</div>
 </body>
 </html>
